433484fda11d3f2648448240a5fc43bad5c9ed8b
Michael <130773647+Shizophrenia@users.noreply.github.com>
2024-02-16 06:36:16 +0300

    Update v10.c

--- Term3/Eighth/v10.c
+++ Term3/Eighth/v10.c
@@ -1,1 +1,54 @@
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int child_main(int argc, char *argv[]) {
+	char fileName[256];
+	int limit;
+	if (scanf("%s %d", fileName, &limit) != 2) {
+		fprintf(stderr, "Fail to read args from input\n");
+		return -1;
+	}
+
+	int inputFile = open(fileName, O_RDONLY);
+	if (inputFile < 0) {
+		printf("Fail to open file\n");
+		return -1;
+	}
+
+	char destinationFileName[strlen(fileName) + 5];
+	strcpy(destinationFileName, fileName);
+	int outputFile = creat(strcat(destinationFileName, ".out"), O_WRONLY);
+	if (outputFile < 0) {
+		printf("Fail to write to %s\n", destinationFileName);
+		return -1;
+	}
+
+	int result = 0;
+	void *handle;
+	handle = dlopen("./libv10.so", RTLD_LOCAL | RTLD_NOW);
+	if (handle) {
+		int (*func)(int, int, int);
+		*(void **)(&func) = dlsym(handle, "replaceLimited");
+		if (func) {
+			result = (*func)(inputFile, outputFile, limit);
+		} else {
+			printf("Failed to find function\n");
+			return -1;
+		}
+		dlclose(handle);
+	} else {
+		printf("Failed to load library\n");
+		return -1;
+	}
+
+	close(inputFile);
+	close(outputFile);
+
+	printf("%d\n", result);
+	return 0;
+}
 
