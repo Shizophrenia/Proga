299b474b566167c9a4e1b71d781a0ad664e841ee
Michael <130773647+Shizophrenia@users.noreply.github.com>
2024-02-16 05:54:51 +0300

    Create v14.c

---
+++ Term3/Fifth/v14.c
@@ -0,0 +1,53 @@
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int child_main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("Wrong arguments count\n");
+		return -1;
+	}
+
+	char *fileName = argv[1];
+	char toReplace = *argv[2];
+
+	int inputFile = open(fileName, O_RDONLY);
+	if (inputFile < 0) {
+		printf("Fail to open file\n");
+		return -1;
+	}
+
+	char destinationFileName[strlen(fileName) + 5];
+	strcpy(destinationFileName, fileName);
+	int outputFile = creat(strcat(destinationFileName, ".out"), O_WRONLY|S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (outputFile < 0) {
+		printf("Fail to write to %s\n", destinationFileName);
+		return -1;
+	}
+
+	int result = 0;
+	void *handle;
+	handle = dlopen("./libv14.so", RTLD_LOCAL | RTLD_NOW);
+	if (handle) {
+		int (*func)(int, int, char);
+		*(void **)(&func) = dlsym(handle, "replaceChar");
+		if (func) {
+			result = (*func)(inputFile, outputFile, toReplace);
+		} else {
+			printf("Failed to find function\n");
+			return -1;
+		}
+		dlclose(handle);
+	} else {
+		printf("Failed to load library\n");
+		return -1;
+	}
+
+	close(inputFile);
+	close(outputFile);
+
+	return result;
+}
