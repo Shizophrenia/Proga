1ec0dc492a726be9aa9dc5b457f4674711cb82f7
Michael <130773647+Shizophrenia@users.noreply.github.com>
2024-02-16 06:35:56 +0300

    Update v10_lib.c

--- Term3/Eighth/v10_lib.c
+++ Term3/Eighth/v10_lib.c
@@ -1,1 +1,19 @@
+#include <unistd.h>
 
+int replaceLimited(int inputFile, int outputFile, int limit) {
+	int replace_count = 0;
+	int first = 0;
+	if (read(inputFile, &first, 1)) write(outputFile, &first, 1);
+	int c = 0;
+	while (read(inputFile, &c, 1)) {
+		if (c == ' ' && limit > 0) {
+			write(outputFile, &first, 1);
+			replace_count++;
+			limit--;
+		} else {
+			write(outputFile, &c, 1);
+		}
+	}
+
+	return replace_count;
+}
